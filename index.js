4df6520ef7741615eb62dafa5d6107652cfb068d
Christer Resin <christer.resin@gmail.com>
2021-11-10 15:26:22 +0100

    make mockup JSON for challenges and maping over challenges arr in obj

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const challengesList = document.querySelector('.challenges-list');
 
+// Use case!
 const renderRatingStars = function (num) {
     let challengeRatingStar = document.createElement('li');
     challengeRatingStar.classList.add('challenge-rating-star on');
@@ -12,68 +13,102 @@ const renderChallenges = function (arr) {
 
     if (arr === undefined || arr.length < 1) {
         let noChallenges = document.createElement('h3');
+        // create new className in CSS and change code to add created className
         noChallenges.classList.add('challenge-title');
         noChallenges.innerHTML = "No matching challenges";
 
         challengesList.appendChild(noChallenges);
-        console.log(arr)
+
     } else {
 
-    for(let i = 0; i < arr.length; i++) {
+        arr.map(obj => {
+            // Create elements for challenge card
+            let challengesItem = document.createElement('li');
+            challengesItem.classList.add('challenges-item');
 
-    // Create elements for challenge card
-    let challengesItem = document.createElement('li');
-    challengesItem.classList.add('challenges-item')
+            let challengesPicture = new Image();
+            challengesPicture.classList.add('challenge-picture');
+            challengesPicture.src=obj.imgUrl;
 
-    let challengesPicture = new Image();
-    challengesPicture.classList.add('challenge-picture')
-    challengesPicture.src="images/challenge.png";
+            let challengeTitle = document.createElement('h3');
+            challengeTitle.classList.add('challenge-title');
+            challengeTitle.innerHTML = obj.title;
 
-    let challengeTitle = document.createElement('h3');
-    challengeTitle.classList.add('challenge-title');
-    challengeTitle.innerHTML = 'Title of room (on-site)'
+            let challengeMeta = document.createElement('div');
+            challengeMeta.classList.add('challenge-meta');
 
-    let challengeMeta = document.createElement('div');
-    challengeMeta.classList.add('challenge-meta');
+            let challengeRating = document.createElement('ul');
+            challengeRating.classList.add('challenge-rating');
 
-    let challengeRating = document.createElement('ul');
-    challengeRating.classList.add('challenge-rating');
+            // Create elements for rating stars
+            for (let i = 0; i < 5; i++) {
+                let challengeRatingStar = document.createElement('li');
+                challengeRatingStar.classList.add('challenge-rating-star');
+                challengeRatingStar.classList.add('on');
+                challengeRating.appendChild(challengeRatingStar);
+            }
 
-    // Create elements for rating stars
-    for (let i = 0; i < 5; i++) {
-        let challengeRatingStar = document.createElement('li');
-        challengeRatingStar.classList.add('challenge-rating-star');
-        challengeRatingStar.classList.add('on');
-        challengeRating.appendChild(challengeRatingStar);
-    }
+            let challengeSize = document.createElement('small');
+            challengeSize.classList.add('challenge-size');
+            challengeSize.innerHTML = `${obj.minParticipants}-${obj.maxParticipants} participants`;
 
-    let challengeSize = document.createElement('small');
-    challengeSize.classList.add('challenge-size');
-    challengeSize.innerHTML = '2-6 participants'
+            let challengeDescription = document.createElement('p');
+            challengeDescription.classList.add('challenge-description');
+            challengeDescription.innerHTML = obj.description
 
-    let challengeDescription = document.createElement('p');
-    challengeDescription.classList.add('challenge-description');
-    challengeDescription.innerHTML = "Reprehenderit voluptate ullamco pariatur labore cupidatat consectetur est adipisicing enim. Deserunt sit irure consequat consequat elit aliquip eiusmod aliquip et ipsum ipsum."
-    
-    let challengeCta = document.createElement('a');
-    challengeCta.classList.add('challenge-cta');
-    challengeCta.innerHTML = "Book the room"
+            let challengeCta = document.createElement('a');
+            challengeCta.classList.add('challenge-cta');
+            challengeCta.innerHTML = "Book the room";
 
-    // Render/append on site
-    challengesList.appendChild(challengesItem);
-    challengesItem.appendChild(challengesPicture);
-    challengesItem.appendChild(challengeTitle);
-    challengesItem.appendChild(challengeMeta)
-    challengeMeta.appendChild(challengeRating);
-    challengeMeta.appendChild(challengeSize);
-    challengesItem.appendChild(challengeDescription);
-    challengesItem.appendChild(challengeCta);
+            // Render/append on site
+            challengesList.appendChild(challengesItem);
+            challengesItem.appendChild(challengesPicture);
+            challengesItem.appendChild(challengeTitle);
+            challengesItem.appendChild(challengeMeta)
+            challengeMeta.appendChild(challengeRating);
+            challengeMeta.appendChild(challengeSize);
+            challengesItem.appendChild(challengeDescription);
+            challengesItem.appendChild(challengeCta);
+                        
+        });
+    };
+};
 
-    }
-    }
+let mockupJson = {
+    challenges: [
+        {
+            title: "Title of challenge",
+            description: "Reprehenderit voluptate ullamco pariatur. Deserunt sit irure consequat consequat elit aliquip eiusmod aliquip et ipsum ipsum.",
+            minParticipants: 2,
+            maxParticipants: 6,
+            rating: 3,
+            imgUrl: "https://raw.githubusercontent.com/richardolsson/lernia-sjj-assignments/main/02-esc-website/images/hero.png",
+            type: 'online',
+            tags: ["Coding", "Linux"]
+        },
+        {
+            title: "Title of room",
+            description: "Reprehenderit voluptate ullamco pariatur. Deserunt sit irure consequat consequat elit aliquip eiusmod aliquip et ipsum ipsum.",
+            minParticipants: 2,
+            maxParticipants: 4,
+            rating: 4,
+            imgUrl: "https://raw.githubusercontent.com/richardolsson/lernia-sjj-assignments/main/02-esc-website/images/hero.png",
+            type: 'online',
+            tags: ["Coding", "Linux"]
+        },
+        {
+            title: "Title of ROOM",
+            description: "Reprehenderit voluptate ullamco pariatur. Deserunt sit irure consequat consequat elit aliquip eiusmod aliquip et ipsum ipsum.",
+            minParticipants: 2,
+            maxParticipants: 4,
+            rating: 3,
+            imgUrl: "https://raw.githubusercontent.com/richardolsson/lernia-sjj-assignments/main/02-esc-website/images/hero.png",
+            type: 'online',
+            tags: ["Coding", "Linux"]
+        }
+    ]
 }
 
-let myArr = [1, 2, 3];
 
-renderChallenges(myArr)
+renderChallenges(mockupJson.challenges);
 
