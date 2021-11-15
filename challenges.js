1b8c354e7bf556695dc33a891b661c88cee471e0
Christer Resin <christer.resin@gmail.com>
2021-11-15 13:44:02 +0100

    convert challenge item to OOP class

--- challenges.js
+++ challenges.js
@@ -3,7 +3,7 @@ const challengesList = document.querySelector('.challenges-list');
 
 // Render function
 const renderChallenges = function (arr) {
- 
+
     // Check that input is array with values
     if (arr === undefined || arr.length < 1) {
         let noChallenges = document.createElement('h3');
@@ -26,64 +26,83 @@ const renderChallenges = function (arr) {
             });
         });
 
+        // OOP Challenge class
+        class Challenge {
+            constructor(data) {
+                this.image = data.image;
+                this.title = data.title;
+                this.rating = data.rating;
+                this.minParticipants = data.minParticipants;
+                this.maxParticipants = data.maxParticipants;
+                this.description = data.description;
+            }
+            render() {
+                    // Create elements for challenge card
+                let challengesItem = document.createElement('li');
+                challengesItem.classList.add('challenges-item');
+
+                let challengesPicture = document.createElement('img')
+                challengesPicture.classList.add('challenge-picture');
+                challengesPicture.src=this.image;
+
+                let challengeTitle = document.createElement('h3');
+                challengeTitle.classList.add('challenge-title');
+                challengeTitle.innerHTML = this.title;
+
+                let challengeMeta = document.createElement('div');
+                challengeMeta.classList.add('challenge-meta');
+
+                let challengeRating = document.createElement('ul');
+                challengeRating.classList.add('challenge-rating');
+
+                // Create elements for rating stars
+                for(let i = 0; i < Math.floor(this.rating); i++) {
+                    let challengeRatingStar = document.createElement('li');
+                    challengeRatingStar.classList.add('challenge-rating-star');
+                    challengeRatingStar.classList.add('on');
+                    challengeRating.appendChild(challengeRatingStar);
+            
+                };
+
+                for(let i = Math.floor(this.rating); i < 5; i++) {
+                    let challengeRatingStar = document.createElement('li');
+                    challengeRatingStar.classList.add('challenge-rating-star');
+                    challengeRatingStar.classList.add('off');
+                    challengeRating.appendChild(challengeRatingStar);
+                };
+
+                let challengeSize = document.createElement('small');
+                challengeSize.classList.add('challenge-size');
+                challengeSize.innerHTML = `${this.minParticipants}-${this.maxParticipants} participants`;
+
+                let challengeDescription = document.createElement('p');
+                challengeDescription.classList.add('challenge-description');
+                challengeDescription.innerHTML = this.description
+
+                let challengeCta = document.createElement('a');
+                challengeCta.classList.add('challenge-cta');
+                challengeCta.innerHTML = "Book the room";
+
+                // Append elements to li element
+                challengesItem.appendChild(challengesPicture);
+                challengesItem.appendChild(challengeTitle);
+                challengesItem.appendChild(challengeMeta)
+                challengeMeta.appendChild(challengeRating);
+                challengeMeta.appendChild(challengeSize);
+                challengesItem.appendChild(challengeDescription);
+                challengesItem.appendChild(challengeCta);
+
+                return challengesItem;
+            }
+        }
+
         // Loops over all filtered objs in arr to render elements on page
         filterArray(arr).map(obj => {
-            // Create elements for challenge card
-            let challengesItem = document.createElement('li');
-            challengesItem.classList.add('challenges-item');
-
-            let challengesPicture = document.createElement('img')
-            challengesPicture.classList.add('challenge-picture');
-            challengesPicture.src=obj.image;
-
-            let challengeTitle = document.createElement('h3');
-            challengeTitle.classList.add('challenge-title');
-            challengeTitle.innerHTML = obj.title;
-
-            let challengeMeta = document.createElement('div');
-            challengeMeta.classList.add('challenge-meta');
-
-            let challengeRating = document.createElement('ul');
-            challengeRating.classList.add('challenge-rating');
-
-            // Create elements for rating stars
-            for(let i = 0; i < Math.floor(obj.rating); i++) {
-                let challengeRatingStar = document.createElement('li');
-                challengeRatingStar.classList.add('challenge-rating-star');
-                challengeRatingStar.classList.add('on');
-                challengeRating.appendChild(challengeRatingStar);
-        
-            };
-
-            for(let i = Math.floor(obj.rating); i < 5; i++) {
-                let challengeRatingStar = document.createElement('li');
-                challengeRatingStar.classList.add('challenge-rating-star');
-                challengeRatingStar.classList.add('off');
-                challengeRating.appendChild(challengeRatingStar);
-            };
-
-            let challengeSize = document.createElement('small');
-            challengeSize.classList.add('challenge-size');
-            challengeSize.innerHTML = `${obj.minParticipants}-${obj.maxParticipants} participants`;
-
-            let challengeDescription = document.createElement('p');
-            challengeDescription.classList.add('challenge-description');
-            challengeDescription.innerHTML = obj.description
-
-            let challengeCta = document.createElement('a');
-            challengeCta.classList.add('challenge-cta');
-            challengeCta.innerHTML = "Book the room";
+            const challengeInstance = new Challenge(obj);
+            const challengesItem = challengeInstance.render();      
 
             // Render/append on site
-            challengesList.appendChild(challengesItem);
-            challengesItem.appendChild(challengesPicture);
-            challengesItem.appendChild(challengeTitle);
-            challengesItem.appendChild(challengeMeta)
-            challengeMeta.appendChild(challengeRating);
-            challengeMeta.appendChild(challengeSize);
-            challengesItem.appendChild(challengeDescription);
-            challengesItem.appendChild(challengeCta);
-                        
+            challengesList.appendChild(challengesItem);             
         });
     };
 };
@@ -94,7 +113,7 @@ let filters = {
     byOnsite: true,
     byLabel: false,
     byRating: false,
-    byText: true
+    byText: false
 };
 
 // Filter variables
