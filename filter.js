2b2d755c86fea7c2c22324d8f2c189657ad15327
Ammar Amin <ammar.amin1975@gmail.com>
2021-11-17 13:45:10 +0100

    create own branch base on main branch

---
+++ filter.js
@@ -0,0 +1,14 @@
+class LabelsGathering {
+    constructor(arr) {
+        this.arr = arr;
+    }
+    var labArr = [];
+    arr.forEach(obj => {
+        obj.labels.forEach(label => {
+            if (!labArr.includes(label)) {
+                labArr.push(label);
+            };
+        });
+    });
+    return labArr;
+};
