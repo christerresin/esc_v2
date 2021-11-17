e97d4c60e3cc7f9cb0000921ce69b9516b6b86e4
matodorovic <martaannatodorovic@gmail.com>
2021-11-17 14:36:05 +0100

    Rating stars working

--- index.js
+++ index.js
@@ -1,173 +1,7 @@
-const challengesList = document.querySelector('.challenges-list');
-
-// Use case!
-const renderRatingStars = function (num) {
-    let challengeRatingStar = document.createElement('li');
-    challengeRatingStar.classList.add('challenge-rating-star on');
-
-}
-
-const renderChallenges = function (arr) {
-
-    
-
-    if (arr === undefined || arr.length < 1) {
-        let noChallenges = document.createElement('h3');
-        // create new className in CSS and change code to add created className
-        noChallenges.classList.add('challenge-title');
-        noChallenges.innerHTML = "No matching challenges";
-
-        challengesList.appendChild(noChallenges);
-
-    } else {
-
-        arr.map(obj => {
-            // Create elements for challenge card
-            let challengesItem = document.createElement('li');
-            challengesItem.classList.add('challenges-item');
-
-            let challengesPicture = new Image();
-            challengesPicture.classList.add('challenge-picture');
-            challengesPicture.src=obj.image;
-
-            let challengeTitle = document.createElement('h3');
-            challengeTitle.classList.add('challenge-title');
-            challengeTitle.innerHTML = obj.title;
-
-            let challengeMeta = document.createElement('div');
-            challengeMeta.classList.add('challenge-meta');
-
-            let challengeRating = document.createElement('ul');
-            challengeRating.classList.add('challenge-rating');
-
-            // Create elements for rating stars
-            for (let i = 0; i < 5; i++) {
-                let challengeRatingStar = document.createElement('li');
-                challengeRatingStar.classList.add('challenge-rating-star');
-                challengeRatingStar.classList.add('on');
-                challengeRating.appendChild(challengeRatingStar);
-            }
-
-            let challengeSize = document.createElement('small');
-            challengeSize.classList.add('challenge-size');
-            challengeSize.innerHTML = `${obj.minParticipants}-${obj.maxParticipants} participants`;
-
-            let challengeDescription = document.createElement('p');
-            challengeDescription.classList.add('challenge-description');
-            challengeDescription.innerHTML = obj.description
-
-            let challengeCta = document.createElement('a');
-            challengeCta.classList.add('challenge-cta');
-            challengeCta.innerHTML = "Book the room";
-
-            // Render/append on site
-            challengesList.appendChild(challengesItem);
-            challengesItem.appendChild(challengesPicture);
-            challengesItem.appendChild(challengeTitle);
-            challengesItem.appendChild(challengeMeta)
-            challengeMeta.appendChild(challengeRating);
-            challengeMeta.appendChild(challengeSize);
-            challengesItem.appendChild(challengeDescription);
-            challengesItem.appendChild(challengeCta);
-                        
-        });
-    };
-};
-
-// renderChallenges(mockupJson.challenges);
-
-// Varje filter motsvaras av en index-plats. Och en funktion kollar om ett "index" / "filter" är på / 1 / true eller av / 0 / false. -- klumpigare än EventListener?
-// Lägg till eventlistener på varje filter - dvs om det är "togglat" -> filterfunktion. Map-funktion för att göra ny array och inte repetera samma objects?
-// Does map function allow to combine different arrays? Nope, you chain them :D Hehe, oki. I think... Yeah, we'll see when we try. Same with .filter()
-// function w/ eventListener == true or false?
-// New array that has gone through filter functions top -> bottom
-
-// ADD and REMOVE filters / filter results according to EventListeners (add and remove eventlistener results) to push / make new array that's dynamic -- e.g. changes "on the fly" with user input (add remove filters)?
-
-// Compare objects in array with objects in another array? To filter...?
-
-// Splice function replaces or removes
-
-//------
-
-// API -> JSON data Obj, Arr, Obj
-
-// API OBJ 15 challenges
-
-// filterByType(array); 
-// filterByRating(array);
-// filterByTags(array);
-// filterByText(array);
-
-let filters = {
-    byOnline: false,
-    byOnsite: false,
-    byLabel: false,
-    byRating: false,
-    byChar: false
-}
-
-// WEBPAGE
-// -----------------------
-// O - Includes online
-
-// (WEB) (LINUX)
-
-// let tags = ['Cryptography', 'Linux'] 
-// let checkTrue = function () {
-
-//     push indexOf(buttonContent) = 1
-//     tags.splice(indexOf('Web', 0, 'Linux')
-
-//     reduce
-
-//     if (tags.length > 0) {
-//         filters.byTag === true
-//     } else {
-        
-//     }
-// }
-
-
-let filterArray = function (array) {
-
-    let filteredArray = array
-    
-    if (filters.byOnline) {
-        filteredArray = filteredArray.filter(obj => {
-            return obj.type === 'online'
-        })
-    }
-
-     else if (filters.byLabel) {
-        filteredArray = filteredArray.filter(obj => {
-            for(let i = 0; i < labels.length; i++) {
-                return obj.labels.includes(tags[i])
-
-            }
-        })
-    }
-
-    console.log(filteredArray)
-    return filteredArray
-}
-
-
-let apiUrl = 'https://lernia-sjj-assignments.vercel.app/api/challenges'
-
-let challengesData = async function () {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    renderChallenges(filterArray(data.challenges))
-}
-
-challengesData()
-
-
+// Filtr box - filter function
 const filterButton = document.querySelector('.filter-cta');
 
-filterButton.addEventListener('click',() => 
+filterButton.addEventListener('click', () => 
     {
         const filterBoard = document.querySelector('.filter-board');
         
@@ -227,7 +61,6 @@ filterButton.addEventListener('click',() =>
         const filterRating = document.createElement('div');
         filterRating.classList.add("filter-by-rating");
         filtersToChoose.appendChild(filterRating);
-        
 
         const filterRatingTitle = document.createElement('h4');
         filterRatingTitle.innerHTML = "By Rating";
@@ -236,19 +69,47 @@ filterButton.addEventListener('click',() =>
         const filterRatingList = document.createElement('ul');
         filterRating.appendChild(filterRatingList);
 
-        for (let i = 0; i < 5; i++) {
-            const filterRatingStar = document.createElement('li');
-            filterRatingStar.classList.add('filter-rating-star');
-            filterRatingStar.classList.add('off');
-            filterRatingList.appendChild(filterRatingStar);
-        }
 
+    const starsArray = [];
+
+        for (let i = 0; i < 5; i++) {
+            starsArray[i] = document.createElement('li');
+            starsArray[i].classList.add("filter-rating-star");
+            starsArray[i].classList.add("off");
+            filterRatingList.appendChild(starsArray[i]); 
+            starsArray.push();
+        } 
+
+        function starRating(starsArray) {
+        const starLength = starsArray.length;
+
+        starsArray.forEach((star) => {
+            star.addEventListener('click', () =>  {
+                let i = starsArray.indexOf(star);
+                
+                if (star.classList.contains("off")) {
+                    for (i; i >= 0; --i) {
+                        starsArray[i].classList.add("on");
+                        starsArray[i].classList.remove("off");
+                    }
+                }
+                else {
+                    for (i; i < starLength; ++i) {
+                        starsArray[i].classList.add("off");
+                        starsArray[i].classList.remove("on");
+                    } 
+                }
+            }); 
+        }); }
+
+        starRating(starsArray);
+    
+        // Label "to" between stars
         const filterStarLabel = document.createElement('li');
         filterStarLabel.innerHTML = "&nbspto&nbsp";
         filterStarLabel.style.textAlign = "center";
         filterStarLabel.style.padding = "0.5rem";
         filterRatingList.appendChild(filterStarLabel);
-
 
         for (let i = 0; i < 5; i++) {
             const filterRatingStar = document.createElement('li');
@@ -257,6 +118,7 @@ filterButton.addEventListener('click',() =>
             filterRatingList.appendChild(filterRatingStar);
         }
 
+        // Tag creation
         const filterTag = document.createElement('div');
         filterTag.classList.add("filter-by-tag");
         filtersToChoose.appendChild(filterTag);
@@ -269,13 +131,10 @@ filterButton.addEventListener('click',() =>
         filterTagLabels.classList.add("filter-by-tag-item");
         filterTag.appendChild(filterTagLabels);
 
-        //for (let i = 0; i < data.challenges.length; i++) {
+        const filterTagItem = document.createElement('label');
+        filterTagItem.innerHTML =  "Web";
+        filterTagLabels.appendChild(filterTagItem);
 
-        //    for (let j = 0; j < data.challenges.labels.length; j++) {
-
-            const filterTagItem = document.createElement('label');
-            filterTagItem.innerHTML = "Web"//data.challenges.labels[j];
-            filterTagLabels.appendChild(filterTagItem);
 
         filterTagItem.addEventListener('click', () => 
             {   
@@ -283,26 +142,18 @@ filterButton.addEventListener('click',() =>
                         filterTagItem.style.backgroundColor = "lightslategray";
                         filterTagItem.style.color = "white";
                         filterTagItem.style.borderColor = "lightslategray";
+                        filters.byLabel = true;
                         
                 }
                 else {
                     filterTagItem.style.backgroundColor = "white";
                     filterTagItem.style.color = "gray";
                     filterTagItem.style.borderColor = "lightgray";
+                    filters.byLabel = false;
                 }
             }    
         );
 
-            
-          // } 
-        //}
-
-        let challengesData = async function () {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        }
-
-        challengesData();
 
         const filterSearchText = document.createElement('div');
         filterSearchText.classList.add("filter-search-text");
@@ -316,6 +167,34 @@ filterButton.addEventListener('click',() =>
         searchText.type = "text";
         searchText.placeholder = "Start typing to filter";
         filterSearchText.appendChild(searchText);
+        
+        
+        filterListPoint1.addEventListener('click', () => {
+            if (!filters.byOnline) {
+                filters.byOnline = true;
+                filterArray(arr);
+            }
+            else {
+                filters.byOnline = false;
+            }
+        });
+
+        filterListPoint2.addEventListener('click', () => {
+            if (!filters.byOnsite) {
+                filters.byOnsite = true;
+            }
+            else {
+                filters.byOnsite = false;
+            }
+        });
+
+        let filters = {
+            byOnline: false,
+            byOnsite: false,
+            byLabel: false,
+            byRating: false,
+            byText: false
+        }
 
         
         filterButton.style.display = 'none'; 
@@ -328,61 +207,3 @@ filterButton.addEventListener('click',() =>
         );
     }
 );
-
-
-
-//----
-
-// .booking {
-//     display: grid;
-//     grid-template: "type rating tags" 1fr
-//                    "search search search" 1fr
-//                    / 1fr 1fr 1fr;
-//   }
-  
-//   @media(max-width: 800px) {
-//     .booking {
-//       grid-template: "a"
-//                      "b"
-//                      "c"
-//                      "d" ;
-//       overflow:auto;
-//     }
-//   }
-  
-//   .booking__img1 {
-//     grid-area: a;
-//   }
-  
-//   .booking__btn {
-//     font: bold 24px Roboto;
-//     color: #ffffff;
-//     background-color: #e3170a;
-//     border-radius: 4px;
-//     padding: 19px 53px;
-//     }
-  
-//   .booking__textBtnWrapper1 {
-//     grid-area: b;
-//     margin: 0;
-//     padding: 60px 96px;
-//   }
-  
-//   .booking__img2 {
-//     grid-area: c;
-//   }
-  
-//   .booking__textBtnWrapper2 {
-//     grid-area: d;
-//     padding: 60px 96px;
-//   }
-  
-//   .booking__description {
-//     font-size: 24px;
-//   }
-  
-//   .booking__imgs {
-//   width:100%;
-//   height:100%;
-//   }
-  
