5cb1a674b980565d5cd69944c0ac8ceba0726260
matodorovic <martaannatodorovic@gmail.com>
2021-11-18 12:49:23 +0100

    fixed conflicts

--- src/modules/ChallengeGrid.js
+++ src/modules/ChallengeGrid.js
@@ -40,73 +40,8 @@ export class ChallengeGrid {
         // Filtr box - filter function
 const filterButton = document.querySelector('.filter-cta');
 
-<<<<<<< HEAD
-filterButton.addEventListener('click', () => 
-    {
-        const filterBoard = document.querySelector('.filter-board');
-        
-        const filterMenu  = document.createElement('div');
-        filterMenu.classList.add("filter-menu");
-        filterBoard.appendChild(filterMenu);
-        
-        const filterBoxTitle = document.createElement('h3');
-        filterBoxTitle.classList.add("filter-box-title");
-        filterMenu.appendChild(filterBoxTitle);
-        filterBoxTitle.innerHTML = "Filter challenges";
-
-        const xButton = document.createElement('a');
-        xButton.classList.add("x-button-style");
-        const btnSpan = document.createElement('p');
-        btnSpan.innerHTML = "&#10005;";
-        xButton.appendChild(btnSpan);
-        filterMenu.appendChild(xButton);
-
-        const filtersToChoose = document.createElement('div');
-        filtersToChoose.classList.add('choose-filter-box');
-        filterMenu.appendChild(filtersToChoose);
-
-        const filterType = document.createElement('div');
-        filterType.classList.add("filter-by-type");
-        filtersToChoose.appendChild(filterType);
-
-        const filterTypeTitle = document.createElement('h4');
-        filterTypeTitle.innerHTML = "By Type";
-        filterType.appendChild(filterTypeTitle);
-
-        const filterTypeList = document.createElement('ul');
-        filterType.appendChild(filterTypeList);
-
-        const filterListPoint1 = document.createElement('li');
-        filterTypeList.appendChild(filterListPoint1);
-
-        const filterTypeCheck1 = document.createElement('input');
-        filterTypeCheck1.type = "checkbox";
-        filterTypeCheck1.checked = true
-        filterListPoint1.appendChild(filterTypeCheck1);
-
-        const filterTypeText1 = document.createElement('label');
-        filterTypeText1.innerHTML = "&nbspInclude online challenges";
-        filterListPoint1.appendChild(filterTypeText1);
-
-        const filterListPoint2 = document.createElement('li');
-        filterTypeList.appendChild(filterListPoint2);
-
-        const filterTypeCheck2 = document.createElement('input');
-        filterTypeCheck2.type = "checkbox";
-        filterTypeCheck2.checked = true
-        filterListPoint2.appendChild(filterTypeCheck2);
-
-        const filterTypeText2 = document.createElement('label');
-        filterTypeText2.innerHTML = "&nbspInclude on-site challenges";
-        filterListPoint2.appendChild(filterTypeText2);
-        
-        const filterRating = document.createElement('div');
-        filterRating.classList.add("filter-by-rating");
-        filtersToChoose.appendChild(filterRating);
-=======
 const filterBoard = document.querySelector('.filter-board');
 filterBoard.style.display = "none";
->>>>>>> marta
 
 
 const filterMenu  = document.createElement('div');
@@ -147,71 +82,6 @@ const filterTypeCheck1 = document.createElement('input');
 filterTypeCheck1.type = "checkbox";
 filterListPoint1.appendChild(filterTypeCheck1);
 
-<<<<<<< HEAD
-        // Creates array of all labels in all challenges
-        this.challenges.forEach(obj => {
-            obj.labels.forEach(label => {
-                if(!this.labelsArray.includes(label)) {
-                    this.labelsArray.push(label);
-                    const filterTagItem = document.createElement('label');
-                    filterTagItem.innerHTML = label;
-                    filterTagItem.style.backgroundColor = 'white';
-                    filterTagLabels.appendChild(filterTagItem);
-                    filterTagItem.addEventListener('click', (event) => {
-                        // Label selected conditional
-                        if (filterTagItem.style.backgroundColor == "white") {
-                            filterTagItem.style.backgroundColor = "lightslategray";
-                            filterTagItem.style.color = "white";
-                            filterTagItem.style.borderColor = "lightslategray";                          
-                        } else {
-                            filterTagItem.style.backgroundColor = "white";
-                            filterTagItem.style.color = "gray";
-                            filterTagItem.style.borderColor = "lightgray";
-                        }
-
-                        const value = event.target.innerText;
-                        const index = this.filters.labelsFilters.indexOf(value);
-                        this.filters.labelsFilters.includes(value) ? this.filters.labelsFilters.splice(index, 1) : this.filters.labelsFilters.push(value);
-                        this.filters.labelsFilters.length > 0 ? this.filters.byLabel = true : this.filters.byLabel = false;
-                        
-                        this.rerender()
-                    })
-                }
-            });
-        });
-
-
-
-        const filterSearchText = document.createElement('div');
-        filterSearchText.classList.add("filter-search-text");
-        filterMenu.appendChild(filterSearchText);
-
-        const filterSearchTextTitle = document.createElement('h4');
-        filterSearchTextTitle.innerHTML = "Or type to search for keyword";
-        filterSearchText.appendChild(filterSearchTextTitle);
-
-        const searchText = document.createElement('input');
-        searchText.type = "text";
-        searchText.classList.add('filter-search-input');
-        searchText.placeholder = "Start typing to filter";
-        searchText.addEventListener('keyup', () => {
-            this.filters.byText = true;
-            this.filters.textFilter = searchText.value;
-            this.rerender()
-        });
-        filterSearchText.appendChild(searchText);
-        
-        
-        
-        filterListPoint1.addEventListener('change', () => {
-            if (!this.filters.byOnline) {
-                this.filters.byOnline = true;
-                this.rerender()
-            }
-            else {
-                this.filters.byOnline = false;
-                this.rerender()
-=======
 const filterTypeText1 = document.createElement('label');
 filterTypeText1.innerHTML = "&nbspInclude online challenges";
 filterListPoint1.appendChild(filterTypeText1);
@@ -260,7 +130,6 @@ starsArray.forEach((star) => {
             for (i; i >= 0; --i) {
                 starsArray[i].classList.add("on");
                 starsArray[i].classList.remove("off");
->>>>>>> marta
             }
         }
         else {                   
@@ -288,20 +157,6 @@ for (let i = 0; i < 5; i++) {
     filterRatingList.appendChild(filterRatingStar);
 }
 
-<<<<<<< HEAD
-        filterListPoint2.addEventListener('change', () => {
-            if (!this.filters.byOnsite) {
-                this.filters.byOnsite = true;
-                this.rerender()
-            }
-            else {
-                this.filters.byOnsite = false;
-                this.rerender()
-            }
-        });
-
-        
-=======
 // Tag creation
 const filterTag = document.createElement('div');
 filterTag.classList.add("filter-by-tag");
@@ -401,7 +256,6 @@ xButton.addEventListener('click',() =>
 
 filterButton.addEventListener('click', () => 
     {
->>>>>>> marta
         filterButton.style.display = 'none'; 
         filterBoard.style.display = "block";
     }
