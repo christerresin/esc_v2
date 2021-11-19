053f378ec6a1f7d438cd3eac9bf915cd8932a93d
Christer Resin <christer.resin@gmail.com>
2021-11-19 09:13:38 +0100

    refactor code

--- src/modules/ChallengeGrid.js
+++ src/modules/ChallengeGrid.js
@@ -38,107 +38,107 @@ export class ChallengeGrid {
 
         // FILTERBOX
         // Filtr box - filter function
-const filterButton = document.querySelector('.filter-cta');
+        const filterButton = document.querySelector('.filter-cta');
 
-filterButton.addEventListener('click', () => 
-    {
-        const filterBoard = document.querySelector('.filter-board');
-        
-        const filterMenu  = document.createElement('div');
-        filterMenu.classList.add("filter-menu");
-        filterBoard.appendChild(filterMenu);
-        
-        const filterBoxTitle = document.createElement('h3');
-        filterBoxTitle.classList.add("filter-box-title");
-        filterMenu.appendChild(filterBoxTitle);
-        filterBoxTitle.innerHTML = "Filter challenges";
+        filterButton.addEventListener('click', () => 
+        {
+            const filterBoard = document.querySelector('.filter-board');
+            
+            const filterMenu  = document.createElement('div');
+            filterMenu.classList.add("filter-menu");
+            filterBoard.appendChild(filterMenu);
+            
+            const filterBoxTitle = document.createElement('h3');
+            filterBoxTitle.classList.add("filter-box-title");
+            filterMenu.appendChild(filterBoxTitle);
+            filterBoxTitle.innerHTML = "Filter challenges";
 
-        const xButton = document.createElement('a');
-        xButton.classList.add("x-button-style");
-        const btnSpan = document.createElement('p');
-        btnSpan.innerHTML = "&#10005;";
-        xButton.appendChild(btnSpan);
-        filterMenu.appendChild(xButton);
+            const xButton = document.createElement('a');
+            xButton.classList.add("x-button-style");
+            const btnSpan = document.createElement('p');
+            btnSpan.innerHTML = "&#10005;";
+            xButton.appendChild(btnSpan);
+            filterMenu.appendChild(xButton);
 
-        const filtersToChoose = document.createElement('div');
-        filtersToChoose.classList.add('choose-filter-box');
-        filterMenu.appendChild(filtersToChoose);
+            const filtersToChoose = document.createElement('div');
+            filtersToChoose.classList.add('choose-filter-box');
+            filterMenu.appendChild(filtersToChoose);
 
-        const filterType = document.createElement('div');
-        filterType.classList.add("filter-by-type");
-        filtersToChoose.appendChild(filterType);
+            const filterType = document.createElement('div');
+            filterType.classList.add("filter-by-type");
+            filtersToChoose.appendChild(filterType);
 
-        const filterTypeTitle = document.createElement('h4');
-        filterTypeTitle.innerHTML = "By Type";
-        filterType.appendChild(filterTypeTitle);
+            const filterTypeTitle = document.createElement('h4');
+            filterTypeTitle.innerHTML = "By Type";
+            filterType.appendChild(filterTypeTitle);
 
-        const filterTypeList = document.createElement('ul');
-        filterType.appendChild(filterTypeList);
+            const filterTypeList = document.createElement('ul');
+            filterType.appendChild(filterTypeList);
 
-        const filterListPoint1 = document.createElement('li');
-        filterTypeList.appendChild(filterListPoint1);
+            const filterListPoint1 = document.createElement('li');
+            filterTypeList.appendChild(filterListPoint1);
 
-        const filterTypeCheck1 = document.createElement('input');
-        filterTypeCheck1.type = "checkbox";
-        filterTypeCheck1.checked = true
-        filterListPoint1.appendChild(filterTypeCheck1);
+            const filterTypeCheck1 = document.createElement('input');
+            filterTypeCheck1.type = "checkbox";
+            filterTypeCheck1.checked = true
+            filterListPoint1.appendChild(filterTypeCheck1);
 
-        const filterTypeText1 = document.createElement('label');
-        filterTypeText1.innerHTML = "&nbspInclude online challenges";
-        filterListPoint1.appendChild(filterTypeText1);
+            const filterTypeText1 = document.createElement('label');
+            filterTypeText1.innerHTML = "&nbspInclude online challenges";
+            filterListPoint1.appendChild(filterTypeText1);
 
-        const filterListPoint2 = document.createElement('li');
-        filterTypeList.appendChild(filterListPoint2);
+            const filterListPoint2 = document.createElement('li');
+            filterTypeList.appendChild(filterListPoint2);
 
-        const filterTypeCheck2 = document.createElement('input');
-        filterTypeCheck2.type = "checkbox";
-        filterTypeCheck2.checked = true
-        filterListPoint2.appendChild(filterTypeCheck2);
+            const filterTypeCheck2 = document.createElement('input');
+            filterTypeCheck2.type = "checkbox";
+            filterTypeCheck2.checked = true
+            filterListPoint2.appendChild(filterTypeCheck2);
 
-        const filterTypeText2 = document.createElement('label');
-        filterTypeText2.innerHTML = "&nbspInclude on-site challenges";
-        filterListPoint2.appendChild(filterTypeText2);
-        
-        const filterRating = document.createElement('div');
-        filterRating.classList.add("filter-by-rating");
-        filtersToChoose.appendChild(filterRating);
+            const filterTypeText2 = document.createElement('label');
+            filterTypeText2.innerHTML = "&nbspInclude on-site challenges";
+            filterListPoint2.appendChild(filterTypeText2);
+            
+            const filterRating = document.createElement('div');
+            filterRating.classList.add("filter-by-rating");
+            filtersToChoose.appendChild(filterRating);
 
-        const filterRatingTitle = document.createElement('h4');
-        filterRatingTitle.innerHTML = "By Rating";
-        filterRating.appendChild(filterRatingTitle);
+            const filterRatingTitle = document.createElement('h4');
+            filterRatingTitle.innerHTML = "By Rating";
+            filterRating.appendChild(filterRatingTitle);
 
-        const filterRatingList = document.createElement('ul');
-        filterRating.appendChild(filterRatingList);
+            const filterRatingList = document.createElement('ul');
+            filterRating.appendChild(filterRatingList);
 
 
-    const starsArray = [];
+        const starsArray = [];
 
-        for (let i = 0; i < 5; i++) {
-            starsArray[i] = document.createElement('li');
-            starsArray[i].classList.add("filter-rating-star");
-            starsArray[i].classList.add("off");
-            filterRatingList.appendChild(starsArray[i]); 
-            starsArray.push();
-        } 
+            for (let i = 0; i < 5; i++) {
+                starsArray[i] = document.createElement('li');
+                starsArray[i].classList.add("filter-rating-star");
+                starsArray[i].classList.add("off");
+                filterRatingList.appendChild(starsArray[i]); 
+                starsArray.push();
+            } 
 
-        function starRating(starsArray) {
-        const starLength = starsArray.length;
+            function starRating(starsArray) {
+            const starLength = starsArray.length;
 
-        starsArray.forEach((star) => {
-            star.addEventListener('click', () =>  {
-                let i = starsArray.indexOf(star);
-                
-                if (star.classList.contains("off")) {
-                    for (i; i >= 0; --i) {
-                        starsArray[i].classList.add("on");
-                        starsArray[i].classList.remove("off");
+            starsArray.forEach((star) => {
+                star.addEventListener('click', () =>  {
+                    let i = starsArray.indexOf(star);
+                    
+                    if (star.classList.contains("off")) {
+                        for (i; i >= 0; --i) {
+                            starsArray[i].classList.add("on");
+                            starsArray[i].classList.remove("off");
+                        }
                     }
-                }
-                else {
-                    for (i; i < starLength; ++i) {
-                        starsArray[i].classList.add("off");
-                        starsArray[i].classList.remove("on");
-                    } 
+                    else {
+                        for (i; i < starLength; ++i) {
+                            starsArray[i].classList.add("off");
+                            starsArray[i].classList.remove("on");
+                        } 
                 }
             }); 
         }); }
