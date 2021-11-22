f126fc403ca426184a84fed821da809c5c180db7
Anders Myrman <amyhrman@gmail.com>
2021-11-22 23:33:37 +0100

    Clean up. Added TODO comments. Finish inserting HTML elements through JS.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// --------DONE------------
+// DONE! addEventListener to click on searchBtn w/ func: display none on bookingmodalcontent1 + display block på bookingmodalcontent2
+// DONE! createElement: h1 innerHTML: 'Book room "title of room" (step 2)'
+// DONE! name, e-mail, what time?, how many participants? input w/ labels
+// DONE! "Submit booking" button w/ eventListener display none on 2, display block on content3
+// DONE! Page 3: "Thank you!"
+// DONE! Link: "Back to challenges"
+
+// --------TODO-----------
+// ADD API FUNCTIONALITY:
+//                          * ALL input fields should send data -> API on button-click
+//                          * ONLY first Search Button has to request data (based on date from bookingModalDateInput), then await response, then display page2 after received
+//                          * add to Eventlistener on bookingModalSearchButton: fetch times from API based on date (with async await?)
+//                          * "What time-input" / bookingModalDateInput4 (page 2) should show available times fetched from API data
+//                          * "Participants-input" / bookingModalDateInput5 (page 2) should show options between min and max participants from API data
+//                          * "Submit booking" should send / submit all page 2 input data to API via HTTP
+
+// Link: "Back to challenges" -- display none on modal container, back to challenges page from landing page?
+// Create a form with input type "submit" to get everything working with API? https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit
+// Change labels to explicit: https://css-tricks.com/html-inputs-and-labels-a-love-story/
+// Styling
+
 // this code block w/ openButton is to be replaced w/ booking-buttons on challenges page + landing page
 let bookingModalOpenButton = document.createElement("button");
 bookingModalOpenButton.classList.add("booking-modal-openButton");
@@ -26,10 +48,11 @@ bookingModalContent1.appendChild(bookingModalDateLabel);
 bookingModalDateLabel.innerHTML = "Date";
 
 let bookingModalDateInput = document.createElement("input");
+bookingModalDateInput.setAttribute('type', 'date');
 bookingModalDateLabel.appendChild(bookingModalDateInput);
 
 let bookingModalSearchButton = document.createElement("button");
-// I guess we should use an already existing button styling?
+// Existing CSS-class w/ button styling?
 bookingModalContent1.appendChild(bookingModalSearchButton);
 bookingModalSearchButton.innerHTML = "Search available times";
 
@@ -38,23 +61,12 @@ let bookingModalContent2 = document.createElement("div");
 bookingModalContent2.classList.add("booking-modal-content2");
 bookingModalContainer.appendChild(bookingModalContent2);
 
-// --------DONE------------
-// DONE! addEventListener to click on searchBtn w/ func: display none on bookingmodalcontent1 + display block på bookingmodalcontent2
-// DONE! createElement: h1 innerHTML: 'Book room "title of room" (step 2)'
-// DONE! name, e-mail, what time?, how many participants? input w/ labels
-// DONE! "Submit booking" button w/ eventListener display none on 2, display block on content3
-// --------TODO------------
-// What time? (fetch data from API), How many participants? (fetch data from API)
-// "Submit booking" button´s eventListener should send booking via HTTP to API
-// Page 3: "Thank you!" (h1 own class - BIG font-size)
-// Link: "Back to challenges" -- (display none on modal container, back to challenges page from landing page)
-
 let bookingModalHeading2 = document.createElement("h1");
 bookingModalContent2.appendChild(bookingModalHeading2);
 bookingModalHeading2.innerHTML = 'Book room "Title of room" (step 2)';
 
 let bookingModalDateLabel2 = document.createElement("label");
-bookingModalContent2.appendChild(bookingModalDateLabel);
+bookingModalContent2.appendChild(bookingModalDateLabel2);
 bookingModalDateLabel2.innerHTML = "Name";
 
 let bookingModalDateInput2 = document.createElement("input");
@@ -65,6 +77,7 @@ bookingModalContent2.appendChild(bookingModalDateLabel3);
 bookingModalDateLabel3.innerHTML = "E-mail";
 
 let bookingModalDateInput3 = document.createElement("input");
+bookingModalDateInput.setAttribute('type', 'email');
 bookingModalDateLabel3.appendChild(bookingModalDateInput3);
 
 let bookingModalDateLabel4 = document.createElement("label");
@@ -88,12 +101,43 @@ bookingModalSubmitButton.innerHTML = "Submit booking";
 
 // --- MODAL PAGE 3
 let bookingModalContent3 = document.createElement("div");
-bookingModalContent2.classList.add("booking-modal-content3");
+bookingModalContent3.classList.add("booking-modal-content3");
 bookingModalContainer.appendChild(bookingModalContent3);
 
 let bookingModalHeading3 = document.createElement('h1');
 bookingModalHeading3.classList.add('booking-modal-heading3')
 bookingModalContent3.appendChild(bookingModalHeading3);
+bookingModalContent3.innerHTML = "Thank you!";
+
+let bookingModalLink = document.createElement('a');
+bookingModalLink.setAttribute('href', 'challenges.html');
+bookingModalContent3.appendChild(bookingModalLink);
+bookingModalLink.innerHTML = "Back to challenges"
+
+//change "bookingModalOpenButton" with whatever class/es booking button from landing page + challenges page has
+bookingModalOpenButton.addEventListener("click", () => {
+  bookingModalContainer.style.display = "block";
+  bookingModalContent1.style.display = "flex";
+  bookingModalContent2.style.display = "none";
+  bookingModalContent3.style.display = "none";
+});
+
+bookingModalSearchButton.addEventListener("click", () => {
+  bookingModalContent1.style.display = "none";
+  bookingModalContent2.style.display = "flex";
+});
+
+bookingModalSubmitButton.addEventListener("click", () => {
+    bookingModalContent2.style.display = "none";
+    bookingModalContent3.style.display = "flex";
+  });
+
+// Close when user clicks on modal container i.e. on overlay / outside modal
+window.addEventListener("click", (event) => {
+  if (event.target == bookingModalContainer) {
+    bookingModalContainer.style.display = "none";
+  }
+});
 
 // --- CHRISTER´S FUNCTION FOR ADDING ELEMENT + CLASS
 // const myFunc = function (elementName, className) {
@@ -104,35 +148,4 @@ bookingModalContent3.appendChild(bookingModalHeading3);
 
 // let modalTest = myFunc("div", 'modalTest');
 // console.log(modalTest);
-// --- END CHRISTER´S FUNCTION
-
-// --- IF WE WANT CLOSEBUTTON
-// let modalCloseBtn = document.querySelector(".modal-closeBtn")
-
-//change "bookingModalOpenButton" with whatever class/es booking button from landing page + challenges page has
-bookingModalOpenButton.addEventListener("click", () => {
-  bookingModalContainer.style.display = "block";
-  bookingModalContent1.style.display = "block";
-  bookingModalContent2.style.display = "none";
-});
-
-bookingModalSearchButton.addEventListener("click", () => {
-  bookingModalContent1.style.display = "none";
-  bookingModalContent2.style.display = "block";
-});
-
-bookingModalSubmitButton.addEventListener("click", () => {
-    bookingModalContent2.style.display = "none";
-    bookingModalContent3.style.display = "block";
-  });
-
-// Close when user clicks on modal container i.e. on overlay / outside modal
-window.addEventListener("click", (event) => {
-  if (event.target == bookingModalContainer) {
-    bookingModalContainer.style.display = "none";
-  }
-});
-
-// modalCloseBtn.addEventListener("click", () => {
-//     modalContainer.style.display = "none"
-// });
+// --- CHRISTER´S FUNCTION END
