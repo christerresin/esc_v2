c8ff4e9f60e07cc29e97a37602dc336688906fc1
Anders Myrman <amyhrman@gmail.com>
2021-11-26 22:05:49 +0100

    Mostly documentation.

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 // --------DONE------------
-// DONE! addEventListener to click on searchBtn w/ func: display none on bookingmodalcontent1 + display block på bookingmodalcontent2
-// DONE! createElement: h1 innerHTML: 'Book room "title of room" (step 2)'
-// DONE! name, e-mail, what time?, how many participants? input w/ labels
-// DONE! "Submit booking" button w/ eventListener display none on 2, display block on content3
-// DONE! Page 3: "Thank you!"
-// DONE! Link: "Back to challenges"
+// addEventListener to click on searchBtn w/ func: display none on bookingmodalcontent1 + display block på bookingmodalcontent2
+// createElement: h1 innerHTML: 'Book room "title of room" (step 2)'
+// name, e-mail, what time?, how many participants? input w/ labels
+// "Submit booking" button w/ eventListener display none on 2, display block on content3
+// Page 3: "Thank you!"
+// Link: "Back to challenges"
+// ONLY first Search Button has to request data (based on date from bookingModalDateInput), then await response, then display page2 after received
+// add to Eventlistener on bookingModalSearchButton: fetch times from API based on date (with async await?)
+// "What time-input" / bookingModalTimeSelect (page 2) should show available times fetched from API data
 
 // --------TODO-----------
-// ADD API FUNCTIONALITY TO MODAL:
-//                          * ALL input fields should send data -> API on button-click
-//                          * ONLY first Search Button has to request data (based on date from bookingModalDateInput), then await response, then display page2 after received
-//                          * add to Eventlistener on bookingModalSearchButton: fetch times from API based on date (with async await?)
-//                          * "What time-input" / bookingModalTimeSelect (page 2) should show available times fetched from API data
-//                          * "Participants-input" / bookingModalPartiInput (page 2) should show options between min and max participants from API data
-//                          * "Submit booking" should send / submit all page 2 input data to API via HTTP
+// *ADD API FUNCTIONALITY TO MODAL:
+//                          ALL input fields should send data -> API on button-click
+//                          "Participants-input" / bookingModalPartiInput (page 2) should show options between min and max participants from API data
+//                          "Submit booking" should send / submit all page 2 input data to API via HTTP
 
 // Link: "Back to challenges" -- display none on modal container, back to challenges page from landing page?
 // Create a form with input type "submit" to get everything working with API? https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit
 // Change labels to explicit: https://css-tricks.com/html-inputs-and-labels-a-love-story/
 // Styling
 
-// Inför 2021-11-25:
-// How to fetch min + maxparticipants from challenge API? Must be fetched on per room basis -- i.e.
-// ANVÄND JSON.STRINGIFY SOM ÄR EN METOD
+
+// *=========| PLAN 2021-11-25: |==========*
+// Regarding POST Check Richards lecture (use json.stringify?) + his post on Slack + https://stackoverflow.com/a/29823632
+// Read up on posting input values. Wrap everything in form-element and use submit on button?
+// How to fetch min + maxparticipants from challenge API? Must be fetched on per room basis -- i.e. when book this button is clicked, it needs to get which room the button connects to?
+// And insert the room name into "Title of room" as well.
+//
+// Optional: fix bookingModalDateInput to type=date with the right date format and min max dates: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date
+// *=======================================*
+
 
 // this code block w/ openButton is to be replaced w/ booking-buttons on challenges page + landing page
-
-// FOR INPUT TYPE DATE: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date
 
 let bookingModalOpenButton = document.createElement("button");
 bookingModalOpenButton.classList.add("booking-modal-openButton");
@@ -55,10 +60,10 @@ bookingModalContent1.appendChild(bookingModalDateLabel);
 bookingModalDateLabel.innerHTML = "Date";
 
 let bookingModalDateInput = document.createElement("input");
-bookingModalDateInput.setAttribute('type', 'date');
+bookingModalDateInput.setAttribute("type", "date");
 bookingModalDateLabel.appendChild(bookingModalDateInput);
-bookingModalDateInput.setAttribute('placeholder', 'YYYY-mm-dd');
-bookingModalDateInput.id = 'inputDate';
+bookingModalDateInput.setAttribute("placeholder", "YYYY-mm-dd");
+bookingModalDateInput.id = "inputDate";
 
 let bookingModalSearchButton = document.createElement("button");
 // Existing CSS-class w/ button styling?
@@ -86,7 +91,7 @@ bookingModalContent2.appendChild(bookingModalEmailLabel);
 bookingModalEmailLabel.innerHTML = "E-mail";
 
 let bookingModalEmailInput = document.createElement("input");
-bookingModalDateInput.setAttribute('type', 'email');
+bookingModalDateInput.setAttribute("type", "email");
 bookingModalEmailLabel.appendChild(bookingModalEmailInput);
 
 let bookingModalTimeLabel = document.createElement("label");
@@ -95,7 +100,7 @@ bookingModalTimeLabel.innerHTML = "What time?";
 
 let bookingModalTimeSelect = document.createElement("select");
 bookingModalTimeLabel.appendChild(bookingModalTimeSelect);
-bookingModalTimeSelect.id = 'selectTime';
+bookingModalTimeSelect.id = "selectTime";
 
 let bookingModalPartiLabel = document.createElement("label");
 bookingModalContent2.appendChild(bookingModalPartiLabel);
@@ -114,15 +119,15 @@ let bookingModalContent3 = document.createElement("div");
 bookingModalContent3.classList.add("booking-modal-content3");
 bookingModalContainer.appendChild(bookingModalContent3);
 
-let bookingModalHeading3 = document.createElement('h1');
-bookingModalHeading3.classList.add('booking-modal-heading3')
+let bookingModalHeading3 = document.createElement("h1");
+bookingModalHeading3.classList.add("booking-modal-heading3");
 bookingModalContent3.appendChild(bookingModalHeading3);
 bookingModalContent3.innerHTML = "Thank you!";
 
-let bookingModalLink = document.createElement('a');
-bookingModalLink.setAttribute('href', 'challenges.html');
+let bookingModalLink = document.createElement("a");
+bookingModalLink.setAttribute("href", "challenges.html");
 bookingModalContent3.appendChild(bookingModalLink);
-bookingModalLink.innerHTML = "Back to challenges"
+bookingModalLink.innerHTML = "Back to challenges";
 
 let inputDate = document.getElementById("inputDate");
 
@@ -136,19 +141,12 @@ let timesData = async () => {
     console.log(data);
     console.log(data.slots);
     addDatesToSelect(data.slots);
-
-    // renderChallenges(data.challenges); -- gör något liknande men en funktion för att skjuta in data.slots -> bookingModalTimeSelect
-
-    } catch (error){
-        console.log(error);
-    }
-
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-// Fetch times from API and add to select options
-
-
-//change "bookingModalOpenButton" with whatever class/es booking button from landing page + challenges page has
+// change "bookingModalOpenButton" with whatever class/es booking button from landing page + challenges page has
 bookingModalOpenButton.addEventListener("click", () => {
   bookingModalContainer.style.display = "block";
   bookingModalContent1.style.display = "flex";
@@ -156,34 +154,34 @@ bookingModalOpenButton.addEventListener("click", () => {
   bookingModalContent3.style.display = "none";
 });
 
-//const addDatesToSelect = (timeArray) => {
+// function to "push" time slots from API to Time selection-element on page 2
 const addDatesToSelect = (timeArray) => {
-  document.getElementById('selectTime').innerHTML = '';
-    timeArray.forEach(item => {
-      const option = document.createElement("option");
-      option.value = item;
-      option.textContent = item;
-      console.log(option.value);
-      document.getElementById('selectTime').appendChild(option);
-    });
-  }
+  document.getElementById("selectTime").innerHTML = "";
+  timeArray.forEach((item) => {
+    const option = document.createElement("option");
+    option.value = item;
+    option.textContent = item;
+    console.log(option.value);
+    document.getElementById("selectTime").appendChild(option);
+  });
+};
 
 //   for (i = 0; i < timeArray.length; i++) {
 //     const option = document.createElement("option");
 //       option.value = timeArray[i];
 //       option.textContent = timeArray[i];
 //   }
-  
+
 bookingModalSearchButton.addEventListener("click", () => {
-    timesData();
+  timesData();
   bookingModalContent1.style.display = "none";
   bookingModalContent2.style.display = "flex";
 });
 
 bookingModalSubmitButton.addEventListener("click", () => {
-    bookingModalContent2.style.display = "none";
-    bookingModalContent3.style.display = "flex";
-  });
+  bookingModalContent2.style.display = "none";
+  bookingModalContent3.style.display = "flex";
+});
 
 // Close when user clicks on modal container i.e. on overlay / outside modal
 window.addEventListener("click", (event) => {
@@ -192,7 +190,7 @@ window.addEventListener("click", (event) => {
   }
 });
 
-// --- CHRISTER´S FUNCTION FOR ADDING ELEMENT + CLASS
+// *--- CHRISTER´S FUNCTION FOR ADDING ELEMENT + CLASS
 // const myFunc = (elementName, className) => {
 //     let element = document.createElement(elementName);
 //     element.classList.add(className);
@@ -201,8 +199,9 @@ window.addEventListener("click", (event) => {
 
 // let modalTest = myFunc("div", 'modalTest');
 // console.log(modalTest);
-// --- CHRISTER´S FUNCTION END
+// *--- CHRISTER´S FUNCTION END
 
+// *Richard´s loadData function
 // async function loadData() {
 //     const url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
 //     const response = await fetch(url);
