c3f10a7fd7235b0139d020eb88d332bee5486072
linama5 <lm.acosta545@gmail.com>
2021-12-08 04:53:36 +0100

    Lina (6) correction

--- src/modules/ChallengeGrid.js
+++ src/modules/ChallengeGrid.js
@@ -156,23 +156,25 @@ export class ChallengeGrid {
             star.addEventListener('click', () => {
                 let i = this.minStarsArray.indexOf(star);
                 let clickedStar = i + 1;
+                if (this.filters.maxRatingFilter >= clickedStar) {
 
-                if(star.classList.contains('off')) {
-                    for (i; i >= 0; i--) {
-                        this.minStarsArray[i].classList.add('on');
-                        this.minStarsArray[i].classList.remove('off');
-                    }
-                } else {
-                    clickedStar--;
-                    for (i; i < this.minStarsArray.length; ++i) {
-                        this.minStarsArray[i].classList.add('off');
-                        this.minStarsArray[i].classList.remove('on');
-                    }
-                };
+                    if(star.classList.contains('off')) {
+                        for (i; i >= 0; i--) {
+                            this.minStarsArray[i].classList.add('on');
+                            this.minStarsArray[i].classList.remove('off');
+                        }
+                    } else {
+                        clickedStar--;
+                        for (i; i < this.minStarsArray.length; ++i) {
+                            this.minStarsArray[i].classList.add('off');
+                            this.minStarsArray[i].classList.remove('on');
+                        }
+                    };
 
-                this.filters.minRatingFilter = clickedStar;
-                this.filters.byRating = true;
-                this.rerender();
+                    this.filters.minRatingFilter = clickedStar;
+                    this.filters.byRating = true;
+                    this.rerender();
+                }
             });
         });
         
@@ -189,23 +191,24 @@ export class ChallengeGrid {
             star.addEventListener('click', () => {
                 let i = this.maxStarsArray.indexOf(star);
                 let clickedStar = i + 1;
+                if (this.filters.minRatingFilter < clickedStar) {
+                    if(star.classList.contains('off')) {
+                        for (i; i >= 0; i--) {
+                            this.maxStarsArray[i].classList.add('on');
+                            this.maxStarsArray[i].classList.remove('off');
+                        }
+                    } else {
+                        clickedStar--;
+                        for (i; i < this.maxStarsArray.length; ++i) {
+                            this.maxStarsArray[i].classList.add('off');
+                            this.maxStarsArray[i].classList.remove('on');
+                        }
+                    };
 
-                if(star.classList.contains('off')) {
-                    for (i; i >= 0; i--) {
-                        this.maxStarsArray[i].classList.add('on');
-                        this.maxStarsArray[i].classList.remove('off');
-                    }
-                } else {
-                    clickedStar--;
-                    for (i; i < this.maxStarsArray.length; ++i) {
-                        this.maxStarsArray[i].classList.add('off');
-                        this.maxStarsArray[i].classList.remove('on');
-                    }
-                };
-
-                this.filters.maxRatingFilter = clickedStar;
-                this.filters.byRating = true;
-                this.rerender();
+                    this.filters.maxRatingFilter = clickedStar;
+                    this.filters.byRating = true;
+                    this.rerender();
+                }
             });
         });
 
